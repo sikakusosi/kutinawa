4d29bee6111bbcd0d32091573517435ba33fb801
sikaku <47070478+sikakusosi@users.noreply.github.com>
2022-10-07 19:31:39 +0900

    Update kutinawa_sandbox.py

--- kutinawa/kutinawa_sandbox.py
+++ kutinawa/kutinawa_sandbox.py
@@ -56,3 +56,29 @@ def generate_alone_defect_pix_map__low_precision(img_size, non_overlap_area, def
 
 
 
+def harris_corner_detector(target_img,window_size,k,th):
+    fil_sobel = [np.array([[ 1, 0,-1],
+                          [ 2, 0,-2],
+                          [ 1, 0,-1], ]),
+                np.array([[-1,-2,-1],
+                          [ 0, 0, 0],
+                          [ 1, 2, 1],]),
+                ]
+
+    blur_img = ndimage.convolve(target_img, wa.generate_gaussian_filter(window_size,np.min(window_size)/3))
+    sobel_img = wa.multi_filter(blur_img,fil_sobel)
+
+    dx2 = sobel_img[0]*sobel_img[0]
+    dy2 = sobel_img[1]*sobel_img[1]
+    dxy = sobel_img[0]*sobel_img[1]
+
+    Sx2 = ndimage.convolve(dx2,np.ones(window_size))
+    Sy2 = ndimage.convolve(dy2,np.ones(window_size))
+    Sxy = ndimage.convolve(dxy,np.ones(window_size))
+
+    S_det = Sx2*Sy2-Sxy*Sxy
+    S_tr  = Sx2+Sy2
+    R_img = S_det - k * (S_tr * S_tr)
+
+    corner_map = R_img>th
+    return corner_map
